aaab2286fe6678fa4660021b7401bebeb31042f9
Hieu Tran <hieutran@HieuTrans-MacBook-Pro.local>
2020-07-16 14:58:22 +0700

    fix-player

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const sizeof = require("object-sizeof");
 
 // # Content Information
 const CONTENT_ID = Base64.encode("*");
-const CONTENT_KEY = Buffer.from("0a237b0752cbf1a827e2fecfb87479a2", "hex").toString("base64")
-const KEY_ID = Buffer.from("d58ce954203b7c9a9a9d467f59839249", "hex").toString("base64")
+const CONTENT_KEY = Buffer.from(
+  "0a237b0752cbf1a827e2fecfb87479a2",
+  "hex"
+).toString("base64");
+const KEY_ID = Buffer.from("d58ce954203b7c9a9a9d467f59839249", "hex").toString(
+  "base64"
+);
 
 // License Values
 const LICENSE_SERVER_URL = "https://license.uat.widevine.com/cenc/getlicense";
@@ -33,29 +38,27 @@ function buildLicenseServerRequest(message_body) {
 
   const request = Buffer.from(message).toString("base64");
   const signature = generateSignature(message);
-  console.log("request ----:", request);
+  // console.log("request ----:", request);
 
-  console.log("signature ----:", signature);
+  // console.log("signature ----:", signature);
 
   return dumps(JSON.stringify({ request, signature, signer: PROVIDER }));
 }
 
 function buildLicenseMessage(message_body) {
-  const payload = Buffer.from(message_body).toString('base64');
+  const payload = Buffer.from(message_body).toString("base64");
   const CONTENT_KEY_SPECS = [{ key_id: KEY_ID, key: CONTENT_KEY }];
-  console.log("CONTENT_KEY_SPECS: ", CONTENT_KEY_SPECS);
+  // console.log("CONTENT_KEY_SPECS: ", CONTENT_KEY_SPECS);
   const message = dumps(
     JSON.stringify({
       content_id: CONTENT_ID,
       content_key_specs: CONTENT_KEY_SPECS,
       payload, //: 'CAES0AsKhwsIARLsCQqvAggCEhGrPdc2zUZgervYHslC4HpiChiEjtfmBSKOAjCCAQoCggEBANn5RTRvUavPYmqKm+H5Z/abA56udXk0veY68Modyrp3cDU/LERX+1/kWo+5ws12/tKoHhrnR+iKQQaFNDHKN19ndfVU9TthI42IpDMjs5Ol8r/zalp1HzdnIT+AsCpbqw2dR/B+RUccyuK04BNmlyMHL2dgn+9cEwXHPNkkj1/sCtw84FvKpqv1y5VTWJ1HAXP6vpL0/Ycf0Zu4Y4hrq4HrLjhmlGz3tb5d3WmWv43vAmWJnELvXBl6L0jTgEVQ+EJwQVrgQLVHl3aXn2DeQjqfi89GRAasU7mxB9H6Yl0SV24/d5vgV7PFbisQHeXdHp6HIEEiFHqlmZ5Q2hjyPgcCAwEAASiEaxKAAlfMZj3h3v6ydkjunj7CRO5FH3w2bMQJ3vxfeyxG0KG9muvwI6jEOzmyu3P6zoqnhh+QIHg4h34mDtWu+KOCPOUSTvC12AD9GYfP1VNQNHwajeAMZLR2XIsFZnGLlrZw0qaan/LB2rsnQwUZtQT24Hn6bDm1WBf/6hMUdNK1aSbz0BCkvdH7Rr2Orkp4gDd6CCofTobijb+MGDhcD9YpKM826JwnIz0i8oJjIWWEh1BtB/jvJiD+nCJBzk1FQ4D5IDahNO0QgqqSAcnEOq3wG7nC6c7xdI5f9X05hvq2E4PEOeasuZlTeLTkfWKYnGiFiZe5eKR11C1QgBcU0112cuQatAUKrgIIARIQLy+FtZELZpt8aqHJlXTjRhjMwOTkBSKOAjCCAQoCggEBAJkkxvZbylBVVcQZQuKlVoO4fm4Nvna1wmnQgG2mqMXwZJevPapaI7phZ9cIiOn/+am8zZcoE/SOUYocqP5OKyuRgQ03QRhrHGuDk+zBY2a6RMNVmHp/7KcCVwRtprmKqoK5h1WLPS/yCOglYzkyKsJVxAWBxDYlFx5Dub6SHlp30EDgsVBupuNufMTP10lkyCXaVnr/O5A3+3e0fW22PNam9A5J+hqhHf9OkBYAlqM+DEnwhJJW1HTEP46h5Qg6/32xB+opea4+CSMjneDH14cnQrBzPjB0ORZCNQ1AqRF3dLBkjCVWY+1PonQUyGELmEhpg5JN3qWxXj/cqCM+MmMCAwEAASiEaxKAAxFSQMO1vN5csywjwSvSjXyiaXo3hHPtoPxXQwc5y1WtItllSj7EyREFJoyMnomYDAFRPU73CNMUI5q7bTN1lgkqs/BTJ4Va2NUW+aQkqjpjxDosjvm3VFgbtjhpTqLxHlSH8T5iep3j8vyjsyH/k4nuqPWoO/FnMk3POQoTZMlzcMS3ScGfxjgD7RaIU9uavJY9g9XUc8BYHRMhWEXZmUMNTxah8V2VwGiHZHDVWJO8KjPSatPGp0Y5ziUEs8zj88qrE2dP6U9KFsnXpnrSK5Ja7aF8wkZCvLsU3SCFbMpZeij4MQrOJlg/uuJgeX6AzM+ElAr+36O+12RjbxBV+vowFSWLrRhtzFcHaDOpjrNcFsJXbMGWcdPMcviO5wQ5pWIHVqiGoLVnbeP7Cd6SqidhSCxFWkqYAjFvubyLIhRwME+aLqNfKvmwdefflpg6QAbp9T/8lBvXGvNC6QIphPPxQRW8dZBb6LAx9yRyavj9sJ1KMqZefqNkgqlD7uSUyxobChFhcmNoaXRlY3R1cmVfbmFtZRIGeDg2LTY0GhYKDGNvbXBhbnlfbmFtZRIGR29vZ2xlGhcKCm1vZGVsX25hbWUSCUNocm9tZUNETRoXCg1wbGF0Zm9ybV9uYW1lEgZNYWNPU1gaIwoUd2lkZXZpbmVfY2RtX3ZlcnNpb24SCzQuMTAuMTUwMy40MggIABAAGAEgABI6CjgKIggBEAEaDXdpZGV2aW5lX3Rlc3QiDXdpZGV2aW5lX3Rlc3QQARoQGpjtTnhsId7CC9lIA6HDEhgBIOeNv/gFMBUagAJk9vJSabrPWwv0LXjgZO4PEteJV4Y8KqN9ZbrEk/x6W5C0+A1L+DcidP43aVDE2F1T6qu0LrCEs89dyMGdkDSpOTchVltzqrQQtgf39lVrL+4yz1W1nK8jL4liKWIB79+YOOSbbTXyePDhuZ8VmGyxNFuQ+PXamxkm+b2Lz0ObeII2EgglA8l4A8WWRGBEN63iYcpkbw14lRzZgdkShY93pFFrHTFSFUmQ1cnqNsLlPHB8yII4WJxhjjQWzz0Ol9ywwwaPLXTjqw9p5H4G5fXS8iLgm8OYyvZbxHyIEUsNFALeWQqCZpaz+cwefEYNrELK81vxkc/Z6yQMcbDWYceT',
-      provider: PROVIDER,
-      // allowed_track_types: ALLOWED_TRACK_TYPES,
+      provider: PROVIDER
     })
   );
   console.log("buildLicenseMessage: ", message);
-  return message
-
+  return message;
 }
 
 function buildCertificateRequest(message_body) {
@@ -67,14 +70,14 @@ function buildCertificateRequest(message_body) {
     signature,
     signer: PROVIDER
   };
-  console.log("certificate_request ----:", certificate_request);
+  // console.log("certificate_request ----:", certificate_request);
   return dumps(JSON.stringify(certificate_request));
 }
 
 function buildCertificateMessage(message_body) {
   let payload = Buffer.from(message_body, "utf-8").toString("base64");
   const request = { payload: "CAQ=" };
-  console.log("Certificate Request ----:", request);
+  // console.log("Certificate Request ----:", request);
   return dumps(JSON.stringify(request));
 }
 
@@ -146,14 +149,12 @@ function getPadding(hashed_text) {
 
 function processLicenseResponse(response) {
   const license_response = JSON.parse(response);
-  console.log('Server response', license_response)
+  console.log("Server response", license_response);
   if (license_response.status == "OK") {
     if (license_response.license)
       return {
         status_ok: true,
-        response: Buffer.from(license_response.license, "base64").toString(
-          "utf-8"
-        )
+        response: Buffer.from(license_response.license, "base64")
       };
     console.log("PARSE_ONLY request, no 'license' found.");
   } else {
@@ -170,7 +171,10 @@ function send500(res, text) {
 }
 
 function dumps(value) {
-  return value.replace(/\:"/g, ': "').replace(/\:\[/g, ': [').replace(/\,"/g, ', "');
+  return value
+    .replace(/\:"/g, ': "')
+    .replace(/\:\[/g, ": [")
+    .replace(/\,"/g, ', "');
 }
 
 const options = {
@@ -190,7 +194,7 @@ app.disable("x-powered-by", false);
 // parse application/json
 // app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.raw());
 var server = http.createServer(app);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -201,56 +205,43 @@ app.use((req, res, next) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.post("/", async (req, res) => {
-  let body = "";
-  req.on("data", chunk => {
-    body += chunk.toString(); // convert Buffer to string
-  });
-  req.on("end", async () => {
-    console.log("body", body);
-    // res.end('ok');
-    if (!body) {
-      console.log("Empty Request");
-      res.status(400).send("Empty Request");
-      return;
-    }
-    let response = null;
-    res.set("Access-Control-Allow-Methods", "POST");
-    res.set("Access-Control-Allow-Credentials", true);
+  let body = req.body;
+  console.log("body", body);
+  // res.end('ok');
+  if (!body) {
+    console.log("Empty Request");
+    res.status(400).send("Empty Request");
+    return;
+  }
+  let response = null;
+  res.set("Access-Control-Allow-Methods", "POST");
+  res.set("Access-Control-Allow-Credentials", true);
 
-    if (!req.referer) res.set("Access-Control-Allow-Origin", "*");
-    else {
-      referer = parse(req.referer);
-      res.set(
-        "Access-Control-Allow-Origin",
-        referer.protocol + "://" + referer.host
-      );
-    }
+  if (!req.referer) res.set("Access-Control-Allow-Origin", "*");
+  else {
+    referer = parse(req.referer);
+    res.set(
+      "Access-Control-Allow-Origin",
+      referer.protocol + "://" + referer.host
+    );
+  }
 
-    try {
-      console.log(
-        "Handle Request: ",
-        sizeof(Base64.encode((body)))
-      );
-      if (sizeof(Base64.encode(body)) < 50) {
-        response = await sendRequest(
-          buildCertificateRequest(body)
-        );
-      } else {
-        response = await sendRequest(
-          buildLicenseServerRequest((body))
-        );
-      }
-      const result = processLicenseResponse(response);
-      console.log("result: ", result);
-      response = result.response;
-      if (result.status_ok) {
-        res.send(response);
-      } else send500(res, response);
-    } catch (err) {
-      console.log(err);
-      send400(res, "Invalid License Request");
+  try {
+    if (sizeof(Base64.encode(body)) < 50) {
+      response = await sendRequest(buildCertificateRequest(body));
+    } else {
+      response = await sendRequest(buildLicenseServerRequest(body));
     }
-  });
+    const result = processLicenseResponse(response);
+    console.log("result: ", result);
+    response = result.response;
+    if (result.status_ok) {
+      res.send(response);
+    } else send500(res, response);
+  } catch (err) {
+    console.log(err);
+    send400(res, "Invalid License Request");
+  }
 });
 
 app.get("/", function(req, res) {
